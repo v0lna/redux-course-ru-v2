ed62d0c00789385d5117c0ddce6da7b95b3e7096
Sergey Lebedinets <graylebedinetss@gmail.com>
2019-10-14 10:18:23 +0300

    same

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from "react"
+import React, { Component } from "react"
 import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Мой топ фото</h1>
+        </header>
+        <p className="App-intro">Здесь будут мои самые залайканые фото</p>
+      </div>
+    )
+  }
 }
 
 export default App
